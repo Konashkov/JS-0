5f34ce50ed79bdf1b969c16170368fb5d3110da1
Konashkov <79665788+Konashkov@users.noreply.github.com>
2022-01-10 17:58:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let message1 = 'first',message2 = 'second',age = 14,year = 2022,messages = 3,isTrue = 3<6,isFalse = 3>6
+console.log(message1)
+console.log(message2)
+console.log(age)
+console.log(year)
+console.log(messages)
+console.log(isTrue)
+console.log(isFalse)
